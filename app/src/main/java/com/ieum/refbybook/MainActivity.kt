360f195a85f2bd4183c651ac29132e65852afecd
Ethan <ethan08liter@gmail.com>
2021-06-14 21:28:33 +0900

    Initial commit

---
+++ app/src/main/java/com/ieum/refbybook/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ieum.refbybook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ieum.refbybook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.btnSay.setOnClickListener {
+            binding.textSay.text = "Hello Kotlin!!"
+        }
+
+    }
+}
